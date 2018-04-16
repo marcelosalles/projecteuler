4801fff0469e39ac5f0811f1eb29a9568754f589
Marcelo <marcelosalles@github.com>
2018-04-16 11:11:22 -0300

    first commit

---
+++ Problem22.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+'''
+Names scores
+Problem 22
+
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order.
+Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+
+'''
+import csv
+
+alfabeto = { 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15,
+			'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+
+def project_euler(file = 'p022_names.txt'):
+
+	csvFile = open(file, 'r')
+	csvReader = csv.reader(csvFile, delimiter=',', quotechar='"')
+
+	for row in csvReader:
+		names = row
+
+	position = 1
+	scores = []
+	for name in sorted(names):
+
+		score = 0
+
+		for letter in name:
+			score += alfabeto[letter]
+
+		scores.append(score*position)
+
+		position += 1
+
+	return(sum(scores))
+
+print(project_euler())
